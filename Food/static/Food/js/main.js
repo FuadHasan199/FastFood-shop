55833a0304ab287227a8a632e3ee527d11a94d49
fuadxyz <fuadhasa199@gmail.com>
2024-03-03 23:18:32 +0600

    more features added

---
+++ Food/static/Food/js/main.js
@@ -0,0 +1,3 @@
+var hours = 24
+var now = new Date.getTime()
+var stepTime = localStorage.getItem('stepTime')
---
+++ Food/static/Food/js/order.js
@@ -0,0 +1,13 @@
+var orders = JSON.parse(localStorage.getItem('orders'))
+var total = localStorage.getItem('total')
+
+if(orders === null || orders === undefined){
+    localStorage.setItem('orders',JSON.stringify([]))
+    orders = JSON.parse(localStorage.getItem('order'))
+}
+if(total === null || total === undefined){
+    localStorage.setItem('total',0)
+    total = localStorage.getItem('total')
+}
+var cart = document.querySelector('#cart')
+cart.innerHTML = orders.length
